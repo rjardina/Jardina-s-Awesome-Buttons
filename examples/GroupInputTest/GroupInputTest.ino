c6424063f45b4029ff6b473d58b8d925d60732e7
Ryan Jardina <59631374+rjardina@users.noreply.github.com>
2025-11-01 10:47:23 -0500

    Update GroupInputTest.ino

--- examples/GroupInputTest/GroupInputTest.ino
+++ examples/GroupInputTest/GroupInputTest.ino
@@ -6,14 +6,15 @@
 // Note: This example was programmed using the PI pico but all Arduino boards should work
 
 #include <Jab.h>                                                                // Library for testing !this! example
-
 Jab GroupOne(INPUT_PULLUP, 2, 3, 4, 5);                                         // 1st group of buttons & used pin numbers (upto 8)
 Jab GroupTwo(INPUT_PULLUP, 16, 17, 18, 19, 20);                                 // 2nd            "                 "
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);                                                 // Output for Heartbeat
   Serial.println(115200); }                                                     // Start serial console
 
 void loop() {
+  digitalWrite(LED_BUILTIN, millis() >> 9 & 1);                                 // Heartbeat to see if code is cycling
   switch (GroupOne.UserInput()) {                                               // Check user's input from group's ONE buttons than goes to that case
     default: case 0: break;                                                     // Do nothing (should never happen in this example) & breaks out of the switch statement
     case 1 ... 255:                                                             // input was in the range from 1 to 255
